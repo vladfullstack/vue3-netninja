a78ba7813918693cda6952857922beefe00dc2af
Vladimir Yardan <vladfullstack@gmail.com>
2022-09-05 11:22:47 +0800

    reaction timer game

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,66 @@
 <template>
-  <h1>{{ title }}</h1>
-  <p>Welcome...</p>
-
-  <teleport to="#modals" v-if="showModal">
-    <Modal theme="sale" @close="toggleModal">
-      <template v-slot:links>
-        <a href="#">sign up now</a>
-        <a href="#">more info</a>
-      </template>
-      <h1>Ninja Givaway!</h1>
-      <p>Grab your ninja swag for half price!</p>
-    </Modal>
-  </teleport>
-
-  <teleport to="#modals" v-if="showModalTwo">
-    <Modal @close="toggleModalTwo">
-      <h1>Sign up to the Newsletter</h1>
-      <p>For updates and promo codes!</p>
-    </Modal>
-  </teleport>
-
-  <button @click.alt="toggleModal">open modal (alt click)</button>
-  <button @click="toggleModalTwo">open modal 2</button>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
-import Modal from "./components/Modal";
+// when the game ends, show the results component
+// output the score inside the results component
+
+import Block from './components/Block'
+import Results from './components/Results'
 
 export default {
-  name: "App",
-  components: { Modal },
+  name: 'App',
+  components: { Block, Results },
   data() {
     return {
-      title: "My First Vue App!",
-      showModal: false,
-      showModalTwo: false,
-    };
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
   },
   methods: {
-    toggleModal() {
-      this.showModal = !this.showModal;
+    start() {
+      // set time amount (ms)
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
     },
-    toggleModalTwo() {
-      this.showModalTwo = !this.showModalTwo;
-    },
-  },
-};
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
+  }
+}
 </script>
 
 <style>
-#app,
-#modals {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-h1 {
-  border-bottom: 1px solid #ddd;
-  display: inline-block;
-  padding-bottom: 10px;
-}
-button {
-  background: #bbb;
-  padding: 10px;
-  border: none;
-  border-radius: 4px;
-  margin: 10px;
-  font-size: 16px;
-  color: #333;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+    margin-top: 60px;
+  }
+  button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+  }
+  button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
 </style>
